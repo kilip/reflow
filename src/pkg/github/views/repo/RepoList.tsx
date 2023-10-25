415eee4b2e6835ac0e87e7ad4ada7f6a8c8b9546
Anthonius Munthi <me@itstoni.com>
2023-10-25 08:02:29 +0800

    refactor: cleaned invalid use of useEffect

--- src/pkg/github/views/repo/RepoList.tsx
+++ src/pkg/github/views/repo/RepoList.tsx
@@ -1,19 +1,22 @@
 'use client'
+import { useThemeContext } from '@/pkg/ui/contexts/ThemeContext'
 import { GitHubSearchItems } from '../../types'
 import RepoCard from './RepoCard'
 
 type Props = {
   repositories: GitHubSearchItems
+  loading: boolean
 }
 
-export default function RepoList({repositories}: Props) {
+export default function RepoList({ repositories, loading }: Props) {
+  if (loading) {
+    return <></>
+  }
+
   return (
-    <div className='flex flex-wrap gap-4 items-start justify-center'>
+    <div className="flex flex-wrap gap-4 items-start justify-center">
       {repositories.map((repo) => (
-        <RepoCard
-          key={repo.id}
-          repo={repo}
-        />
+        <RepoCard key={repo.id} repo={repo} />
       ))}
     </div>
   )
